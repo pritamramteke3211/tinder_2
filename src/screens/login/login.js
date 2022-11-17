3280bea65163a989e30855fe7d54aab216594e21
Pritam Ramteke <pritamramteke3211@gmail.com>
2022-11-17 22:47:40 +0530

    My Second Commit

--- src/screens/login/login.js
+++ src/screens/login/login.js
@@ -19,8 +19,7 @@ const Login = () => {
     // Check if your device supports Google Play
     await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
     // Get the users ID token
-    const { idToken } = await GoogleSignin.signIn();
-  
+    const { idToken } = await GoogleSignin.signIn();  
     // Create a Google credential with the token
     const googleCredential = auth.GoogleAuthProvider.credential(idToken);
   
