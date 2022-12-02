8a88b8a273bd5dbf421c5dc0bfe714757fba6fe5
Pritam Ramteke <pritamramteke3211@gmail.com>
2022-12-03 00:16:29 +0530

    More Changes

---
+++ components/Header.js
@@ -0,0 +1,15 @@
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Header = ({ title, callEnabled }) => {
+  
+    return (
+    <View style={{flexDirection:'row', alignItems:'center',justifyContent:'space-between'}}>
+      <Text>Header</Text>
+    </View>
+  )
+}
+
+export default Header
+
+const styles = StyleSheet.create({})
--- src/screens/chat/chat.js
+++ src/screens/chat/chat.js
@@ -1,11 +1,13 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import Header from '../../../components/Header';
 
 const Chat = () => {
   return (
-    <View>
+    <SafeAreaView>
       <Text>Chat</Text>
-    </View>
+      <Header title="Chat"/>
+    </SafeAreaView>
   );
 };
 
